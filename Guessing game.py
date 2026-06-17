d3dd7cd33ab6f8d3d02e9bdc6671825818b7b0b0
oviya chawla <oviyachawla19@gmail.com>
2026-06-17 10:20:41 +0100

    Updated code

--- Guessing game.py
+++ Guessing game.py
@@ -6,11 +6,15 @@ i=0
 while True:
     i+=1
     guess_number = int(input("Take a guess!\n"))
-    if(guess_number<random_number):
+    if(guess_number<random_number and guess_number!=67):
         print("Your guess is too low.")
-    elif(guess_number>random_number):
+    elif(guess_number>random_number and guess_number!=67):
         print("Your guess is too high.")
+    elif(guess_number== 67):
+        print("SIXSEVEN, you win!")
+        break
     else:
         print('Good job! Your guessed my number in {} guesses!'.format(i))
         break
+
     
